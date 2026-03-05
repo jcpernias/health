34e7102be0a5787228d195c1d1d1d2c0e2f5eb1b
José Pernías <jcpernias@users.noreply.github.com>
2026-03-05 16:49:27 +0100

    Initial commit

---
+++ mkdata.R
@@ -0,0 +1,5 @@
+
+# Read path to raw data files.
+# The file `raw-data-path.txt` should contain the path to the
+# folder where the raw data files are.
+raw_data_path <- readLines("raw-data-path.txt")
