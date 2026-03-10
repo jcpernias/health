7c7433c15c396c0bb42b89cbe9dce76ebc08352f
José Pernías <jcpernias@users.noreply.github.com>
2026-03-10 08:54:44 +0100

    Read household data

---
+++ raw/mkdata-hh.R
@@ -0,0 +1,81 @@
+
+# Read path to raw data files.
+# The file `raw-data-path.txt` should contain the path to the
+# folder where the raw data files are.
+raw_data_path <- readLines("raw-data-path.txt")
+
+library(dplyr)
+library(purrr)
+library(readr)
+library(glue)
+
+# Get position of variables in th households data files
+household_vars <- read_csv("household-vars.csv", col_types = "ciii") |>
+  mutate(end = start + len - 1)
+
+
+# Read household data for a given year
+household_yr <- function(year) {
+  vars_yr <- household_vars |>
+    filter(.data$year == .env$year)
+  positions <- fwf_positions(start = vars_yr$start,
+                             end = vars_yr$end,
+                             col_names = vars_yr$var)
+  types <- cols(EDAD = "i", NADULTOS = "i", NMENORES = "i",
+                .default = "c")
+  db_yr <- read_fwf(file.path(raw_data_path, glue("hogar_{year}.txt.xz")),
+                    col_positions = positions,
+                    col_types = types) |>
+    mutate(year = year,
+           id = glue("{IDENTHOGAR}-{year}")) |>
+    select(-IDENTHOGAR)
+}
+
+# Read household data for all years
+year <- c(2014, 2017, 2020, 2023)
+household_db <- map(year, household_yr) |>
+  bind_rows()
+
+# Number of households by year
+household_db |>
+  distinct(id, .keep_all = TRUE) |>
+  count(year)
+
+# Extract common variables across household members
+household_common <-
+  local({
+    # Drop redundant records and recode NAs
+    db <- household_db |>
+      select(-c(SEXO, EDAD, A10, A11)) |>
+      distinct() |>
+      mutate(INGRESOS = replace_values(INGRESOS, c("98", "99") ~ NA),
+             CLASE_PR = replace_values(CLASE_PR, "9" ~ NA))
+
+    # Find households with more than one record
+    reps <- db |> count(id)
+
+    # Find households with NAs
+    na_inc <- db |>
+      group_by(id) |>
+      summarise(na_inc = sum(is.na(INGRESOS)))
+
+    na_class <- db |>
+      group_by(id) |>
+      summarise(na_class = sum(is.na(CLASE_PR)))
+
+    db |>
+      left_join(reps, join_by(id)) |>
+      left_join(na_inc, join_by(id)) |>
+      left_join(na_class, join_by(id)) |>
+      filter_out(when_any(n == 2 & na_inc == 1 & is.na(INGRESOS),
+                          n == 2 & na_class == 1 & is.na(CLASE_PR))) |>
+      select(-c(n, na_class, na_inc))
+  })
+
+
+# Variables for family members
+individuals_db <- household_db |>
+  select(SEXO, EDAD, A10, A11, id, year) |>
+  mutate(A10 = replace_values(A10, c("98", "99") ~ NA),
+         A11 = replace_values(A11, "9" ~ NA))
+
